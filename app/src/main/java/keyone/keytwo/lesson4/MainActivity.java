27998cd0c14f24c99918a4529eb1f842e3af6318
Andej Nesterenko <mentatus.n@gmail.com>
2021-06-21 20:44:31 +0300

    Первый комит, чистое приложение

--- app/src/main/java/keyone/keytwo/lesson4/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson4/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String myString = getResources().getString(R.string.hello_text);
     }
 }
