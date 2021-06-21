4b0924d676c8c5524d91da068b54ef633b0d0273
Andej Nesterenko <mentatus.n@gmail.com>
2021-06-21 21:00:59 +0300

    Научились правильно создавать папку assets

--- app/src/main/java/keyone/keytwo/lesson4/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson4/MainActivity.java
@@ -2,7 +2,10 @@ package keyone.keytwo.lesson4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String myString = getResources().getString(R.string.hello_text);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/19659.ttf");
+        TextView textView = findViewById(R.id.textView);
+        textView.setTypeface(tf);
+
+        textView.setText(myString);
+
+
     }
 }
